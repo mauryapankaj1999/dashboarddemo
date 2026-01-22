19c3297a69441e207de0d85c4575e8be3be6d528
mauryapankaj1999 <pankajmaurya4610@gmail.com>
2026-01-22 17:12:22 +0530

    add chnage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Dashboard from './pages/dashboard'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
